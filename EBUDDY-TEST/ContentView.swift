0ab24c3965ecdb603ee9b0e9cd3852846be78e34
eric <ericfrndo@gmail.com>
2024-12-20 13:50:16 +0700

    Initial Commit

---
+++ EBUDDY-TEST/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EBUDDY-TEST
+//
+//  Created by Eric Fernando on 20/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ EBUDDY-TEST/EBUDDY_TESTApp.swift
@@ -0,0 +1,17 @@
+//
+//  EBUDDY_TESTApp.swift
+//  EBUDDY-TEST
+//
+//  Created by Eric Fernando on 20/12/24.
+//
+
+import SwiftUI
+
+@main
+struct EBUDDY_TESTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
